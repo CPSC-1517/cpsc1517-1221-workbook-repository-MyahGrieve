21c6d457dfb71ba1d173c1684c121ba5d0f7f1eb
MyahGrieve <90411894+MyahGrieve@users.noreply.github.com>
2022-09-13 10:04:43 -0600

    Add files via upload

---
+++ cpsc1517-1221-workbook-repository-swunait/hockeyTeamMyah/hockeyTeamMyah/NhlDivision.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hockeyTeamMyah
+{
+  
+    public enum NhlDivision
+    {
+        Metropolitan,
+        Atlantic,
+        Central,
+        Pacific
+    }
+}
